96245322bad86664b39c79b887683a1744436c22
Reggen123 <belinskiyilya@mail.ru>
2024-09-20 15:34:08 +0700

    Этот Class - просто класс

--- Labs/lab_1.py
+++ Labs/lab_1.py
@@ -1,272 +1,279 @@
 # data_alphabet = {}
 
+class Encryptor:
+    def __init__(self, alphabet):
+        """
+        Класс, отвечающий за шифрование.
 
-def get_letter_by_id(data_alphabet, id):
-    '''
-    Получает символ алфавита по индексу
+        :param alphabet: Алфавит.
+        """
+        self.data_alphabet = self.add_alphabet(alphabet)
+    def get_letter_by_id(self, id):
+        '''
+        Получает символ алфавита по индексу
 
-    :param data_alphabet: Словарь с алфавитом
-    :param id: Индекс символа
-    :return: Символ
-    '''
-    for i in data_alphabet.keys():
-        if id == data_alphabet[i]:
-            return i
-
-
-def shift_letter_by_number(data_alphabet, letter, shift_value):
-    '''
-    Получает смещённый символ алфавита
-
-    :param data_alphabet: Словарь с алфавитом
-    :param letter: Буква
-    :param shift_value: Смещение
-    :return: Символ
-    '''
-    return get_letter_by_id(data_alphabet, ((data_alphabet[letter] + shift_value) % len(data_alphabet.keys())))
+        :param data_alphabet: Словарь с алфавитом
+        :param id: Индекс символа
+        :return: Символ
+        '''
+        for i in self.data_alphabet.keys():
+            if id == self.data_alphabet[i]:
+                return i
 
 
-def add_alphabet(data_alphabet, text):
-    '''
-    Нумерует каждый символ и превращает в алфавит
+    def shift_letter_by_number(self, letter, shift_value):
+        '''
+        Получает смещённый символ алфавита
 
-    :param data_alphabet: Словарь для записи алфавита
-    :param text: Строка для посимвольного превращения в алфавит
-    '''
-    for i in range(len(text)):
-        data_alphabet[text[i]] = i
-
-
-def text2array(data_alphabet, text):
-    '''
-    Кодирование текста в виде массива индексов алфавита
-
-    :param data_alphabet: Словарь с алфавитом
-    :param text: Текст для кодирования
-    :return: Массив с индексами символов
-    '''
-    arr_return = []
-    for i in text:
-        arr_return.append(data_alphabet[i])
-    return arr_return
+        :param letter: Буква
+        :param shift_value: Смещение
+        :return: Символ
+        '''
+        return self.get_letter_by_id(((self.data_alphabet[letter] + shift_value) % len(self.data_alphabet.keys())))
 
 
-def array2text(data_alphabet, array):
-    '''
-    Декодирует массив индексов алфавита в символы алфавита
+    def add_alphabet(self, text):
+        '''
+        Нумерует каждый символ и превращает в алфавит
 
-    :param data_alphabet: Алфавит
-    :param array: Массив для декодирования
-    :return: Декодированная строка
-    '''
-    text_return = ""
-    for i in array:
-        text_return = text_return + get_letter_by_id(data_alphabet, i)
-    return text_return
-
-
-def add_letters(data_alphabet, letter_a, letter_b):
-    '''
-    Получает символ из суммы индексов двух букв
-
-    :param data_alphabet: Алфавит
-    :param letter_a: Первая буква
-    :param letter_b: Вторая буква
-    :return: Буква, получаемая в результате сложения
-    '''
-
-    let_a_id = data_alphabet[letter_a]
-    let_b_id = data_alphabet[letter_b]
-    letter_id = (let_a_id + let_b_id) % len(data_alphabet.keys())
-    return get_letter_by_id(data_alphabet, letter_id)
+        :param data_alphabet: Словарь для записи алфавита
+        :param text: Строка для посимвольного превращения в алфавит
+        '''
+        data_alphabet = {}
+        for i in range(len(text)):
+            data_alphabet[text[i]] = i
+        return data_alphabet
 
 
-def sub_letters(data_alphabet, letter_a, letter_b):
-    '''
-    Получает символ из вычетания индексов двух букв
+    def text2array(self, text):
+        '''
+        Кодирование текста в виде массива индексов алфавита
 
-    :param data_alphabet: Алфавит
-    :param letter_a: Первая буква
-    :param letter_b: Вычитаемая, вторая буква
-    :return: Буква, получаемая в результате вычитания
-    '''
-
-    let_a_id = data_alphabet[letter_a]
-    let_b_id = data_alphabet[letter_b]
-    letter_id = (let_a_id - let_b_id + len(data_alphabet.keys())) % len(data_alphabet.keys())
-    return get_letter_by_id(data_alphabet, letter_id)
+        :param data_alphabet: Словарь с алфавитом
+        :param text: Текст для кодирования
+        :return: Массив с индексами символов
+        '''
+        arr_return = []
+        for i in text:
+            arr_return.append(self.data_alphabet[i])
+        return arr_return
 
 
-def caesar_encode(data_alphabet, text_to_cypher, key):
-    '''
-    Кодирование шифром Цезаря
+    def array2text(self, array):
+        '''
+        Декодирует массив индексов алфавита в символы алфавита
 
-    :param data_alphabet: Алфавит
-    :param text_to_cypher: Зашифровываемый текст
-    :param key: Ключ, от которого будет использован первый символ
-    :return: Шифротекст
-    '''
-    caesar_key = key[0]
-    text_to_return = ""
-    for l in text_to_cypher:
-        text_to_return += add_letters(data_alphabet, l, caesar_key)
-    return text_to_return
+        :param data_alphabet: Алфавит
+        :param array: Массив для декодирования
+        :return: Декодированная строка
+        '''
+        text_return = ""
+        for i in array:
+            text_return = text_return + self.get_letter_by_id(i)
+        return text_return
 
 
-def caesar_decode(data_alphabet, text_to_decypher, key):
-    '''
-    Расшифровывает шифротекст, созданный с применением шифра Цезаря
+    def add_letters(self, letter_a, letter_b):
+        '''
+        Получает символ из суммы индексов двух букв
 
-    :param data_alphabet: Алфавит
-    :param text_to_decypher: Шифротекст
-    :param key: Ключ, от которого будет использован первый символ
-    :return: Декодированный текст
-    '''
-    caesar_key = key[0]
-    text_to_return = ""
-    for l in text_to_decypher:
-        text_to_return += sub_letters(data_alphabet, l, caesar_key)
-    return text_to_return
+        :param data_alphabet: Алфавит
+        :param letter_a: Первая буква
+        :param letter_b: Вторая буква
+        :return: Буква, получаемая в результате сложения
+        '''
 
-
-def shift_alphabet(data_alphabet, text_to_shift, shift_value):
-    '''
-    Смещает весь текст на заданное количество позиций
-
-    :param data_alphabet: Алфавит
-    :param text_to_shift: Строка для смещения по алфавиту
-    :param shift_value: Значение смещения
-    :return: Строка, соответствующая смещённому алфавиту
-    '''
-    text_to_return = ""
-    for l in text_to_shift:
-        text_to_return += shift_letter_by_number(data_alphabet, l, shift_value)
-    return text_to_return
+        let_a_id = self.data_alphabet[letter_a]
+        let_b_id = self.data_alphabet[letter_b]
+        letter_id = (let_a_id + let_b_id) % len(self.data_alphabet.keys())
+        return self.get_letter_by_id(letter_id)
 
 
-def vidgener_encode(value, key, data_alphabet, j_in):
-    '''
-    Зашифровывает текст при помощи шифра Виженера
+    def sub_letters(self, letter_a, letter_b):
+        '''
+        Получает символ из вычетания индексов двух букв
 
-    :param value: Зашифровываемый текст
-    :param key: Ключ
-    :param data_alphabet: Алфавит
-    :param j_in: Смещение
-    :return: Шифротекст
-    '''
-    lis = ""
-    t_k = " "
-    for v in range(len(value)):
-        t_i = value[v]
-        q = (v + j_in) % len(key)
-        t_k = add_letters(data_alphabet, t_k, key[q])
-        go = add_letters(data_alphabet, t_i, t_k)
-        lis += go
-    return lis
+        :param data_alphabet: Алфавит
+        :param letter_a: Первая буква
+        :param letter_b: Вычитаемая, вторая буква
+        :return: Буква, получаемая в результате вычитания
+        '''
+
+        let_a_id = self.data_alphabet[letter_a]
+        let_b_id = self.data_alphabet[letter_b]
+        letter_id = (let_a_id - let_b_id + len(self.data_alphabet.keys())) % len(self.data_alphabet.keys())
+        return self.get_letter_by_id(letter_id)
 
 
-def vidgener_decode(value, key, data_alphabet, j_in):
-    """
-    Расшифровывает текст, закодированный при помощи шифра Виженера
+    def caesar_encode(self, text_to_cypher, key):
+        '''
+        Кодирование шифром Цезаря
 
-    :param value: Шифротекст
-    :param key: Ключ
-    :param data_alphabet: Алфавит
-    :param j_in: Смещение
-    :return: Расшифрованная строка
-    """
-    lis = ""
-    t_k = " "
-    for v in range(len(value)):
-        t_i = value[v]
-        q = (v + j_in) % len(key)
-        t_k = add_letters(data_alphabet, t_k, key[q])
-        go = sub_letters(data_alphabet, t_i, t_k)
-        lis += go
-    return lis
+        :param data_alphabet: Алфавит
+        :param text_to_cypher: Зашифровываемый текст
+        :param key: Ключ, от которого будет использован первый символ
+        :return: Шифротекст
+        '''
+        caesar_key = key[0]
+        text_to_return = ""
+        for l in text_to_cypher:
+            text_to_return += self.add_letters(l, caesar_key)
+        return text_to_return
 
 
-def s_block_encode(data_alphabet, block_text, key, shift):
-    """
-    Кодирует S-блоки по 4 символа
+    def caesar_decode(self, text_to_decypher, key):
+        '''
+        Расшифровывает шифротекст, созданный с применением шифра Цезаря
 
-    :param data_alphabet: Алфавит
-    :param block_text: Блок текста
-    :param key: Ключ
-    :param shift: Смещение
-    :return: Закодированный блок
-    """
-    if len(block_text) != 4:
-        return "Ошибка: длина входного блока должна быть равна 4"
-    else:
-        ret = ""
-        k = len(key)
-        t_k = get_letter_by_id(data_alphabet, 0)
-        for i in range(4):
-            q = (shift + i) % k
-            t_k = add_letters(data_alphabet, t_k, key[q])
-            ret += add_letters(data_alphabet, block_text[i], t_k)
-        return ret
+        :param data_alphabet: Алфавит
+        :param text_to_decypher: Шифротекст
+        :param key: Ключ, от которого будет использован первый символ
+        :return: Декодированный текст
+        '''
+        caesar_key = key[0]
+        text_to_return = ""
+        for l in text_to_decypher:
+            text_to_return += self.sub_letters(l, caesar_key)
+        return text_to_return
 
 
-def s_block_decode(data_alphabet, encoded_block_text, key, initial_shift):
-    """
-    Расшифровывает s-блок размером 4 символа
+    def shift_alphabet(self, text_to_shift, shift_value):
+        '''
+        Смещает весь текст на заданное количество позиций
 
-    :param data_alphabet: Алфавит
-    :param encoded_block_text: Шифротекст
-    :param key: Ключ
-    :param initial_shift: Изначальное смещение, второй ключ
-    :return: Расшифрованный текст
-    """
-    if len(encoded_block_text) != 4:
-        return "Ошибка: длина входного блока должна быть равна 4"
-    else:
-        ret = ""
-        k = len(key)
-        t_k = get_letter_by_id(data_alphabet, 0)
-        for i in range(4):
-            q = (initial_shift + i) % k
-            t_k = add_letters(data_alphabet, t_k, key[q])
-            ret += sub_letters(data_alphabet, encoded_block_text[i], t_k)
-        return ret
+        :param data_alphabet: Алфавит
+        :param text_to_shift: Строка для смещения по алфавиту
+        :param shift_value: Значение смещения
+        :return: Строка, соответствующая смещённому алфавиту
+        '''
+        text_to_return = ""
+        for l in text_to_shift:
+            text_to_return += self.shift_letter_by_number(l, shift_value)
+        return text_to_return
 
 
-def fwd_improve_block(data_alphabet, block, key, initial_shift):
-    t = key
-    while initial_shift > len(t) - 4:
-        t = t*2
-    key = t[initial_shift:initial_shift+4]
-    k = text2array(data_alphabet, key)
-    b = text2array(data_alphabet, block)
-    q = (k[0] + k[1] + k[2] + k[3])%4
-    for i in range(3):
-        j = (q+i+1) % 4
-        l = (q+i) % 4
-        b[j] = (b[j] + b[l]) % 32
-    return array2text(data_alphabet, b)
+    def vidgener_encode(self, value, key, j_in):
+        '''
+        Зашифровывает текст при помощи шифра Виженера
+
+        :param value: Зашифровываемый текст
+        :param key: Ключ
+        :param j_in: Смещение
+        :return: Шифротекст
+        '''
+        lis = ""
+        t_k = " "
+        for v in range(len(value)):
+            t_i = value[v]
+            q = (v + j_in) % len(key)
+            t_k = self.add_letters(t_k, key[q])
+            go = self.add_letters(t_i, t_k)
+            lis += go
+        return lis
 
 
-def inv_improve_block(data_alphabet, block, key, initial_shift):
-    t = key
-    while initial_shift > len(t) - 4:
-        t = t*2
-    key = t[initial_shift:initial_shift+4]
-    k = text2array(data_alphabet, key)
-    b = text2array(data_alphabet, block)
-    q = (k[0] + k[1] + k[2] + k[3])%4
-    for i in range(2, -1, -1):
-        j = (q+i+1) % 4
-        l = (q+i) % 4
-        b[j] = (b[j] - b[l] + 32) % 32
-    return array2text(data_alphabet, b)
+    def vidgener_decode(self, value, key, j_in):
+        """
+        Расшифровывает текст, закодированный при помощи шифра Виженера
+
+        :param value: Шифротекст
+        :param key: Ключ
+        :param data_alphabet: Алфавит
+        :param j_in: Смещение
+        :return: Расшифрованная строка
+        """
+        lis = ""
+        t_k = " "
+        for v in range(len(value)):
+            t_i = value[v]
+            q = (v + j_in) % len(key)
+            t_k = self.add_letters(t_k, key[q])
+            go = self.sub_letters(t_i, t_k)
+            lis += go
+        return lis
 
 
-def s_block_encode_modified(data_alphabet, block, key, initial_shift):
-    tmp = s_block_encode(data_alphabet, block, key, initial_shift)
-    return fwd_improve_block(data_alphabet, tmp, key, initial_shift)
+    def s_block_encode(self, block_text, key, shift):
+        """
+        Кодирует S-блоки по 4 символа
+
+        :param data_alphabet: Алфавит
+        :param block_text: Блок текста
+        :param key: Ключ
+        :param shift: Смещение
+        :return: Закодированный блок
+        """
+        if len(block_text) != 4:
+            return "Ошибка: длина входного блока должна быть равна 4"
+        else:
+            ret = ""
+            k = len(key)
+            t_k = self.get_letter_by_id(0)
+            for i in range(4):
+                q = (shift + i) % k
+                t_k = self.add_letters(t_k, key[q])
+                ret += self.add_letters(block_text[i], t_k)
+            return ret
 
 
-def s_block_decode_modified(data_alphabet, block, key, initial_shift):
-    tmp = s_block_decode(data_alphabet, block, key, initial_shift)
-    return inv_improve_block(data_alphabet, tmp, key, initial_shift)
+    def s_block_decode(self, encoded_block_text, key, initial_shift):
+        """
+        Расшифровывает s-блок размером 4 символа
+
+        :param data_alphabet: Алфавит
+        :param encoded_block_text: Шифротекст
+        :param key: Ключ
+        :param initial_shift: Изначальное смещение, второй ключ
+        :return: Расшифрованный текст
+        """
+        if len(encoded_block_text) != 4:
+            return "Ошибка: длина входного блока должна быть равна 4"
+        else:
+            ret = ""
+            k = len(key)
+            t_k = self.get_letter_by_id(0)
+            for i in range(4):
+                q = (initial_shift + i) % k
+                t_k = self.add_letters(t_k, key[q])
+                ret += self.sub_letters(encoded_block_text[i], t_k)
+            return ret
+
+
+    def fwd_improve_block(self, block, key, initial_shift):
+        t = key
+        while initial_shift > len(t) - 4:
+            t = t*2
+        key = t[initial_shift:initial_shift+4]
+        k = self.text2array(key)
+        b = self.text2array(block)
+        q = (k[0] + k[1] + k[2] + k[3])%4
+        for i in range(3):
+            j = (q+i+1) % 4
+            l = (q+i) % 4
+            b[j] = (b[j] + b[l]) % len(self.data_alphabet)
+        return self.array2text(b)
+
+
+    def inv_improve_block(self, block, key, initial_shift):
+        t = key
+        while initial_shift > len(t) - 4:
+            t = t*2
+        key = t[initial_shift:initial_shift+4]
+        k = self.text2array(key)
+        b = self.text2array(block)
+        q = (k[0] + k[1] + k[2] + k[3])%4
+        for i in range(2, -1, -1):
+            j = (q+i+1) % 4
+            l = (q+i) % 4
+            b[j] = (b[j] - b[l] + len(self.data_alphabet)) % len(self.data_alphabet)
+        return self.array2text(b)
+
+
+    def s_block_encode_modified(self, block, key, initial_shift):
+        tmp = self.s_block_encode(block, key, initial_shift)
+        return self.fwd_improve_block(tmp, key, initial_shift)
+
+
+    def s_block_decode_modified(self, block, key, initial_shift):
+        tmp = self.s_block_decode(block, key, initial_shift)
+        return self.inv_improve_block(tmp, key, initial_shift)
--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-from Labs import lab_1 as shift
+from Labs.lab_1 import *
 
-alphabet = {}
+
+shift = Encryptor(" АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ")
 
 print('[Алфавит]')
-shift.add_alphabet(alphabet, " АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ")
-print(alphabet)
+print(shift.data_alphabet)
 
 print('\n[Работа с алфавитом]')
-text_as_arr = shift.text2array(alphabet, "ВСЕМ ПРИВЕТ")
+text_as_arr = shift.text2array("ВСЕМ ПРИВЕТ")
 print(text_as_arr)
-print(shift.array2text(alphabet, text_as_arr))
-print(shift.add_letters(alphabet, "Я", "Ж"))
-print(shift.sub_letters(alphabet, "Е", "Ж"))
+print(shift.array2text(text_as_arr))
+print(shift.add_letters("Я", "Ж"))
+print(shift.sub_letters("Е", "Ж"))
 
 print('\n[Цезарь]')
-print(shift.caesar_encode(alphabet, "ПОЛДЕНЬ", "ВЕРСАЛЬ")) # Должно получиться ТСОЗИРЯ
-print(shift.caesar_decode(alphabet, shift.caesar_encode(alphabet, "ПОЛДЕНЬ", "ВЕРСАЛЬ"), "ВЕРСАЛЬ")) # Должно получиться ПОЛДЕНЬ
-print(shift.caesar_encode(alphabet, "СЫЗРАНЬ", "А")) # Должно получиться ТЬИСБОЭ
-print(shift.caesar_encode(alphabet, "ОЛОЛО КРИНЖ", "Х")) # Должно получиться ДБДБДХАЖЯГЭ
+print(shift.caesar_encode("ПОЛДЕНЬ", "ВЕРСАЛЬ")) # Должно получиться ТСОЗИРЯ
+print(shift.caesar_decode( shift.caesar_encode("ПОЛДЕНЬ", "ВЕРСАЛЬ"), "ВЕРСАЛЬ")) # Должно получиться ПОЛДЕНЬ
+print(shift.caesar_encode("СЫЗРАНЬ", "А")) # Должно получиться ТЬИСБОЭ
+print(shift.caesar_encode( "ОЛОЛО КРИНЖ", "Х")) # Должно получиться ДБДБДХАЖЯГЭ
 
 print('\n[Смещение алфавита]')
-print(shift.shift_alphabet(alphabet, " АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ", 1))
-print(shift.shift_alphabet(alphabet, " АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ", -1))
-print(shift.shift_alphabet(alphabet, "ПОЛДЕНЬ", 3)) # Должно получиться ТСОЗИРЯ
+print(shift.shift_alphabet(" АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ", 1))
+print(shift.shift_alphabet(" АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ", -1))
+print(shift.shift_alphabet("ПОЛДЕНЬ", 3)) # Должно получиться ТСОЗИРЯ
 
 print('\n[Виженер]')
-print(shift.vidgener_encode("ОЛОЛО КРИНЖ", "ПАНТЕОН", alphabet, 0)) # Должно получиться ЯЭНЮЖЖ ХОБН
-print(shift.vidgener_decode(shift.vidgener_encode("ОЛОЛО КРИНЖ", "ПАНТЕОН", alphabet, 1), "ПАНТЕОН", alphabet, 1)) # Должно получиться ОЛОЛО КРИНЖ
+print(shift.vidgener_encode("ОЛОЛО КРИНЖ", "ПАНТЕОН", 0)) # Должно получиться ЯЭНЮЖЖ ХОБН
+print(shift.vidgener_decode(shift.vidgener_encode("ОЛОЛО КРИНЖ", "ПАНТЕОН", 1), "ПАНТЕОН", 1)) # Должно получиться ОЛОЛО КРИНЖ
 
 print('\n[S-блоки]')
-print(shift.s_block_encode(alphabet, "БЛО", "ЗВЕЗДНАЯ НОЧЬ", 11)) # Ошибка
-print(shift.s_block_encode(alphabet, "БЛОК", "ЗВЕЗДНАЯ НОЧЬ", 11)) # Должно получиться Щ КЙ
+print(shift.s_block_encode("БЛО", "ЗВЕЗДНАЯ НОЧЬ", 11)) # Ошибка
+print(shift.s_block_encode("БЛОК", "ЗВЕЗДНАЯ НОЧЬ", 11)) # Должно получиться Щ КЙ
 # Должно получиться БЛОК
-print(shift.s_block_decode(alphabet, shift.s_block_encode(alphabet, "БЛОК", "ЗВЕЗДНАЯ НОЧЬ", 11), "ЗВЕЗДНАЯ НОЧЬ", 11))
+print(shift.s_block_decode(shift.s_block_encode("БЛОК", "ЗВЕЗДНАЯ НОЧЬ", 11), "ЗВЕЗДНАЯ НОЧЬ", 11))
 
 print('\n[Усиленные S-блоки]')
-print(shift.fwd_improve_block(alphabet, "АТОЛ", "ГОРАЦИО", 2))
-print(shift.fwd_improve_block(alphabet, "АТОЛ", "ГОРАЦИО", 3))
+print(shift.fwd_improve_block("АТОЛ", "ГОРАЦИО", 2))
+print(shift.fwd_improve_block("АТОЛ", "ГОРАЦИО", 3))
 
-print(shift.inv_improve_block(alphabet, shift.fwd_improve_block(alphabet, "АТОЛ", "ГОРАЦИО", 2), "ГОРАЦИО", 2))
-print(shift.inv_improve_block(alphabet, shift.fwd_improve_block(alphabet, "АТОЛ", "ГОРАЦИО", 3), "ГОРАЦИО", 3))
+print(shift.inv_improve_block(shift.fwd_improve_block("АТОЛ", "ГОРАЦИО", 2), "ГОРАЦИО", 2))
+print(shift.inv_improve_block(shift.fwd_improve_block("АТОЛ", "ГОРАЦИО", 3), "ГОРАЦИО", 3))

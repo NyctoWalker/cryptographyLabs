f9546fcea36df581492d98f12cc2fc9b3652fd8f
NyctoWalker <115701336+NyctoWalker@users.noreply.github.com>
2024-11-05 15:06:35 +0700

    Шаблон для четвёртой лабораторной

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # from Tests.lab_1_tests import *
 # from Tests.lab_2_tests import *
 # from Tests.lab_3_tests import *
+
+from Labs.lab_3 import *
+from Labs.lab_4 import *
+
+encoder = BinaryEncryptor(" АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ")
+generator = SPNet(encoder)
+encryptor = Lab4Temp()

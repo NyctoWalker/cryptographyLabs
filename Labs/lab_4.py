1c1ca2d91178723f638bace031e386438ff8cf9c
NyctoWalker <115701336+NyctoWalker@users.noreply.github.com>
2024-11-11 19:35:46 +0700

    Разметка оставшихся методов(без тестов)
    Переписаны все методы из справочных скринов, однако без тестов и с заглушками вместо некоторых функций

--- Labs/lab_4.py
+++ Labs/lab_4.py
@@ -78,40 +78,152 @@ class Lab4Temp:
 
     # region Подложки
 
-    def check_padding(self, bin_msg_in):
+    @staticmethod
+    def check_padding(bin_msg_in):
         _m = len(bin_msg_in)
         blocks_num = _m // 80
         remainder = _m % 80
+        f = 0  # Так как там много выходов, которые пишут f=0, и отдельно f=1 с модификациями
+        pad_length = 0  # А вот тут вообще если remainder != 0, этот параметр не задаётся
+        numblocks = 0
         if remainder == 0:
-            # Я сдаюсь, не смог разобраться что берётся в submatrix
-            pass
+            # Проверка: если удалить filler, подсветка синтаксиса скажет что такой переменной нет ниже
+            filler = [0, 0, 0]  # Заменить на операции с матрицами если поймём что это такое.
+            tb = filler
+            ender = filler
+            if ender == [0, 0, 1]:
+                NB = filler
+                PL = filler
+                for i in range(7):
+                    pad_length = 2*pad_length + PL[i]
+                for i in range(10):
+                    numblocks = 2*numblocks + NB[i]
+                if numblocks == blocks_num and (103 > pad_length >= 23):
+                    tb = filler
+                    starter = tb[0]
+                    if starter == 1:
+                        f = 1
+                        for j in range(1, pad_length):
+                            tmp = tb[j]
+                            if tmp == 1:
+                                f = 0
+                            break  # Вообще без понятия зачем цикл если потом в любом случае используем break
+        return [f, numblocks, pad_length]
 
-    def produce_padding(self):
-        pass
+    @staticmethod
+    def produce_padding(rem_in, blocks_in):
+        b = blocks_in + 1
+        if rem_in == 0:
+            r = 80
+        elif rem_in <= 57:
+            r = 80 - rem_in
+        else:
+            b += 1
+            r = 160 - rem_in
+        pad = "1"
 
-    def pad_message(self):
-        pass
+        for i in range(1, r-21):
+            pad += "0"
 
-    def unpad_message(self):
-        pass
+        rt = r
+        for i in range(6, -1, -1):
+            pad[r-20+i] = rt % 2  # Тут может быть проблемно, так как pad то строка/лист, динамически ничего не добавляется как в маткаде
+            rt = rt // 2
+        for i in range(9, -1, -1):
+            pad[r-12+i] = b % 2  # налогично прошлому циклу, по индексу мы не обратимся
+            b = b // 2
+        pad[r-3], pad[r-2] = "0", "0"
+        pad[r-1] = "1"
+        return pad
+
+    def pad_message(self, msg_in):
+        pad = ""
+        bins = self.msg2bin(msg_in)
+        _m = len(bins)
+        blocks = _m // 80
+        rem = _m % 80
+        f = self.check_padding(bins)[0] if rem == 0 else 1
+
+        if f == 1:
+            pad = self.produce_padding(rem, blocks)
+            for i in range(len(pad)):
+                bins[_m + i] = pad[i]  # Тут могут быть проблемы
+        return self.bin2msg(bins)
+
+    def unpad_message(self, msg_in):
+        bins = self.msg2bin(msg_in)
+        _m = len(bins)
+        t = self.check_padding(bins)
+        if t[0] == 1:
+            filler = [0, 0, 0]  # Аналогично такому же филлеру выше
+            pl = t[1]  # ??? я не понял то за индексы
+            tmp = filler
+            return self.bin2msg(tmp)
+        else:  # Оно тут просто для наглядности
+            return msg_in
 
     # endregion
 
     # region Пакеты
 
-    def prepare_packet(self):
-        pass
+    def prepare_packet(self, data_in, iv_in, msg_in):
+        iv = self.encoder.add_block('                ', iv_in)  # Предполагаю что там 16 пробелов
+        msg = self.pad_message(msg_in)
+        _l = len(self.msg2bin(msg))
+        a = ""
 
-    def validate_packet(self):
-        pass
+        # Метод не дописан, нужно раскомментировать а
+        for i in range(5):
+            # a = self.num2sym + a  # Не уверен, что именно за num2sym и как в concat влияет порядок
+            _l //= 32
+        data_in[4] = a
+        return [data_in, iv, msg, ""]
 
-    def transmit(self):
-        pass
+    @staticmethod
+    def validate_packet(packet_in):
+        data, iv, msg, mac = packet_in
+        f = 1  # Возможно, его вообще не стоит задавать
+        #  Не уверен, что к data можно обращаться по этим индексам
+        t = data[0][0]
+        s = data[0][1]
+        ml = len(mac)
 
-    def receive(self):
-        pass
+        #  Можно объединить в большой if и заменить сразу return 0
+        if t != "В":
+            f = 0
+        elif ml != 16 and (s == "А" or s == "Б"):
+            f = 0
+        elif ml != 0 and s == " ":
+            f = 0
+        return f  # Если сверху будет заменено на return 0, тот можно return 1
 
-    # Возможно, не нужно реализовывать
+    def transmit(self, packet_in):
+        data, iv, msg, mac = packet_in
+        out = data[0] + data[1] + data[2] + data[3] + data[4]
+        return self.msg2bin(out + iv + msg + mac)
+
+    def receive(self, stream_in):
+        p = self.bin2msg(stream_in)
+        _m = len(p)
+        #  Возможно, тут я не так написал срезы и нужно "подвинуть" на один индекс
+        _type = p[:2]
+        _sender = p[2:10]
+        _receiver = p[10:18]
+        _session = p[18:27]
+        _length = p[27:32]
+        _iv = p[32:48]
+        _l = 0
+        #  Что-то нужно будет раскомментировать
+        for i in range(5):
+            t = _length[i]
+            # l = sym2num  # Проверить что такое sym2num и где это у нас
+            # _l =32*L + l
+        _l //= 5
+        message = p[48:48 + _l]
+        mac = p[48 + _l: _m - (48 + _l)]
+        return [[_type, _sender, _receiver, _session, _length], _iv, message, mac]
+
+    # Возможно, не нужно реализовывать и это уже есть
     def textor(self):
         pass
 
@@ -119,16 +231,68 @@ class Lab4Temp:
 
     # region CCM
 
-    def enc_CTR(self):
-        pass
+    def enc_CTR(self, msg_in, iv_in, key_in, r_in):
+        m = len(msg_in) // 16
+        iv_starter = iv_in[:12]
+        iv_ender = "    "  # Предполагая что там 4 символа
+        ctr = 0
+        out = ""
+        for i in range(m):
+            iv_ender = self.encoder.number_to_block(ctr)
+            iv = iv_starter + iv_ender
+            # Возможно тут какой-то другой метод
+            keystream = self.encoder.s_block_encode_modified(iv, key_in, r_in)
+            inp = msg_in[i*16:i*16 + 16]
+            # textxor
+            # out += textxor(inp, keystream)
+            ctr += 1
+        return out
 
-    def mac_CBC(self):
-        pass
+    def mac_CBC(self, msg_in, iv_in, key_in, r_in):
+        m = len(msg_in) // 16
+        ctr = 0
+        out = ""
+        for i in range(m):
+            inp = msg_in[i*16:i*16 + 16]
+            # textxor
+            # tmp = textxor(iv_in, imp)
+            # Возможно тут какой-то другой метод + tmp
+            # iv_in = self.encoder.s_block_encode_modified(tmp, key_in, r_in)
+            out += iv_in
+        return iv_in  # Не out?
 
-    def CCM_frw(self):
-        pass
+    def CCM_frw(self, packet_in, key_in, only_mac):
+        assdata_in, iv_in, msg_in, tmp = packet_in
+        data = ""  # Изменить тип в зависимости от типа данных в assdata_in
+        for ad in assdata_in:
+            data += ad
+        _m = len(msg_in)
+        mac = self.mac_CBC(data+msg_in, iv_in, key_in)
+        if only_mac == 0:
+            msg = self.enc_CTR(msg_in+mac, iv_in, key_in)
+            _msg = msg[0:_m]
+            _mac = msg[_m:_m + 16]
+        else:
+            _msg = msg_in
+            _mac = mac
+        return [assdata_in, iv_in, _msg, _mac]
 
-    def CCM_inv(self):
-        pass
+    def CCM_inv(self, packet_in, key_in, only_mac):
+        assdata_in, iv_in, msg_in, mac_in = packet_in
+        data = ""  # Изменить тип в зависимости от типа данных в assdata_in
+        for ad in assdata_in:
+            data += ad
+        _m = len(msg_in)
+        if only_mac == 0:
+            msg = self.enc_CTR(msg_in+mac_in, iv_in, key_in)
+            _msg = msg[0:_m]
+            _mac = msg[_m:_m + 16]
+        else:
+            _msg = msg_in
+            _mac = mac_in
+        mac = self.mac_CBC(data + _msg, iv_in, key_in)
+        # textxor
+        # _mac = textxor(_mac, mac)
+        return [assdata_in, iv_in, _msg, _mac]
 
     # endregion
